f7297da156e9966ec2430934b667f0496a0d283c
Rajat Pathania <83996978+pathania0001@users.noreply.github.com>
2025-06-22 10:57:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your code here
+  const button = document.getElementById("open-modal")
+  const modal = document.getElementById("newton-modal")
+  const closebtns = document.getElementsByClassName("cross");
+
+   Array.from(closebtns).forEach((btn)=>btn.addEventListener("click",closeModal));
+   button.onclick = openModal;
+function openModal() {
+  modal.classList.add("show");
+  modal.style.display = "block";
+  modal.removeAttribute("aria-hidden");
+  modal.setAttribute("aria-modal", "true");
+}
+
+function closeModal() {
+  modal.classList.remove("show");
+  modal.style.display = "none";
+  modal.setAttribute("aria-hidden", "true");
+  modal.removeAttribute("aria-modal");
+}
